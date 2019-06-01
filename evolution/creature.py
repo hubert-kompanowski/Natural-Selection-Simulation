4193ca238c62324547dc31bd7e393b92b1acb51d
hubert-kompanowski <hubert.kompanowski9@gmail.com>
2019-06-01 14:08:56 +0200

    change location of creatures

--- evolution/creature.py
+++ evolution/creature.py
@@ -6,12 +6,15 @@ import time
 
 
 class Creature:
-    def __init__(self, x_, y_):
+    def __init__(self, x_, y_, index_):
+        self.index = index_
         self.pos = (x_, y_)
         self.start_pos = (x_, y_)
         self.velocity = 5.0
         self.actual_step = 0
         self.move_plan = []
+        self.actual_step_to_home = 0
+        self.move_home_plan = []
 
         self.range = 600
         self.max_energy = 100.0
@@ -26,31 +29,44 @@ class Creature:
         self.meal_id = -1
 
     def move(self):
+
+        print(str(self.index), end=" is ")
+        if not self.is_alive:
+            print("dead ", end=", ")
+        else:
+            print("alive", end=", ")
         if self.returned:
+            print("    returned", end=", ")
+        else:
+            print("not returned", end=", ")
+        if self.returning:
+            print("    returning", end=", ")
+        else:
+            print("not returning", end=", ")
+        print("eat " + str(self.eaten_meals) + " meals")
+
+        if self.returned:
+            self.returning = False
             return -1
 
-        if self.actual_energy <= 0:
+        if self.returning:
+            self.move_home()
+            return -1
+
+        if self.actual_energy <= 0 and self.eaten_meals == 0:
             self.is_alive = False
             return -1
 
-
-
-        # if len(self.move_plan) == 1:
-        #     self.pos = self.move_plan[0]
+        if (self.actual_energy <= 0 and self.eaten_meals == 1) and not self.returning:
+            self.returning = True
+            self.is_alive = False
+            self.find_path_to_home()
 
         if self.actual_step + 1 == len(self.move_plan):
             self.eaten_meals += 1
             if self.eaten_meals == 2:
                 self.is_eaten = True
-            if self.returning:
-                self.returned = True
-                self.returning = False
             return self.meal_id
-
-        if self.returning:
-            self.actual_step += 1
-            self.pos = self.move_plan[self.actual_step]
-            return -1
 
         if self.actual_step + 1 < len(self.move_plan):
             self.actual_step += 1
@@ -61,19 +77,30 @@ class Creature:
 
         return -1
 
+    def move_home(self):
+        if self.pos[0] == self.start_pos[0] and self.pos[1] == self.start_pos[1]:
+            self.returned = True
+            self.returning = False
+
+        else:
+            self.actual_step_to_home += 1
+            self.pos = self.move_home_plan[self.actual_step_to_home]
+
     def move_rand(self):
         pass
 
     def search(self, meals):
+        print("search")
 
-        if (self.is_eaten and self.is_alive) or (not self.is_alive and self.eaten_meals == 1) or (
+        if len(meals) == 0 and self.eaten_meals == 0:
+            self.is_alive = False
+            self.actual_energy = 0.0
+            self.returning = False
+            return None
+
+        if (self.is_eaten and self.is_alive) or (self.actual_energy <= 0 and self.eaten_meals == 1) or (
                 len(meals) == 0 and self.eaten_meals > 0):
-            step_to_achieve = 15
-            dest_x = self.start_pos[0]
-            dest_y = self.start_pos[1]
-            self.meal_id = -1
-            self.is_eaten = True
-            self.returning = True
+            self.find_path_to_home()
 
         elif self.eaten_meals < 2 and self.is_alive:
             self.move_plan.clear()
@@ -101,12 +128,32 @@ class Creature:
             dest_y = meals[index].y
 
             self.meal_id = meals[index].id
-        else:
-            return None
+
+            for i in range(1, step_to_achieve + 1):
+                x_next_step = self.pos[0] + math.floor((dest_x - self.pos[0]) / step_to_achieve * i)
+                y_next_step = self.pos[1] + math.floor((dest_y - self.pos[1]) / step_to_achieve * i)
+                self.move_plan.append((x_next_step, y_next_step))
+
+            self.move_plan.append((dest_x, dest_y))
+
+        # else:
+
+
+
+    def find_path_to_home(self):
+        self.actual_step_to_home = 0
+        self.move_home_plan.clear()
+
+        step_to_achieve = 15
+        dest_x = self.start_pos[0]
+        dest_y = self.start_pos[1]
+        self.meal_id = -1
+        self.is_eaten = True
+        self.returning = True
 
         for i in range(1, step_to_achieve + 1):
             x_next_step = self.pos[0] + math.floor((dest_x - self.pos[0]) / step_to_achieve * i)
             y_next_step = self.pos[1] + math.floor((dest_y - self.pos[1]) / step_to_achieve * i)
-            self.move_plan.append((x_next_step, y_next_step))
+            self.move_home_plan.append((x_next_step, y_next_step))
 
-        self.move_plan.append((dest_x, dest_y))
+        self.move_home_plan.append((dest_x, dest_y))
--- evolution/start.py
+++ evolution/start.py
@@ -14,8 +14,8 @@ class Evolution:
         self.map_size = 800
         self.meal_map_size = (200, 600)
         self.cr_pos = self.map_size / 2
-        self.creatures_number = 2
-        self.meals_numbers = 3
+        self.creatures_number = 20
+        self.meals_numbers = 15
         self.screen = pygame.display.set_mode((self.map_size, self.map_size))
         self.screen.fill(WHITE)
         pygame.display.update()
@@ -29,21 +29,21 @@ class Evolution:
             if i % 4 == 0:
                 y_pos = self.map_size / 2 - self.cr_pos / 2
                 self.creatures.append(
-                    Creature(10, math.floor(y_pos + i // 4 * self.cr_pos / (self.creatures_number / 4))))
+                    Creature(10, math.floor(y_pos + i // 4 * self.cr_pos / (self.creatures_number / 4)), i))
             if i % 4 == 1:
                 y_pos = self.map_size / 2 - self.cr_pos / 2
                 self.creatures.append(
                     Creature(self.map_size - 10,
-                             math.floor(y_pos + i // 4 * self.cr_pos / (self.creatures_number / 4))))
+                             math.floor(y_pos + i // 4 * self.cr_pos / (self.creatures_number / 4)), i))
             if i % 4 == 2:
                 x_pos = self.map_size / 2 - self.cr_pos / 2
                 self.creatures.append(
-                    Creature(math.floor(x_pos + i // 4 * self.cr_pos / (self.creatures_number / 4)), 10))
+                    Creature(math.floor(x_pos + i // 4 * self.cr_pos / (self.creatures_number / 4)), 10, i))
             if i % 4 == 3:
                 x_pos = self.map_size / 2 - self.cr_pos / 2
                 self.creatures.append(
                     Creature(math.floor(x_pos + i // 4 * self.cr_pos / (self.creatures_number / 4)),
-                             self.map_size - 10))
+                             self.map_size - 10, i))
 
     def day(self):
         self.meals = [Meal(self.screen, self.meal_map_size, i) for i in range(1, self.meals_numbers + 1)]
@@ -59,7 +59,7 @@ class Evolution:
 
         to_drop = []
         for c in self.creatures:
-            if not c.is_alive:
+            if not c.is_alive and c.eaten_meals == 0:
                 to_drop.append(c)
 
             c.actual_energy = c.max_energy
@@ -69,7 +69,9 @@ class Evolution:
             c.is_alive = True
             c.eaten_meals = 0
             c.actual_step = 0
+            c.actual_step_to_home = 0
             c.move_plan = []
+            c.move_home_plan = []
 
         for d in to_drop:
             self.creatures.remove(d)
@@ -82,11 +84,10 @@ class Evolution:
         for m in self.meals:
             m.draw()
 
-        all_return_or_dead = True
         for c in self.creatures:
-            if (not c.returned and c.is_alive) or c.returning:
-                all_return_or_dead = False
+
             eaten_meal_id = c.move()
+
             if c.is_alive:
                 pygame.draw.circle(self.screen, BLUE, c.pos, 10)
             elif c.eaten_meals == 1:
@@ -101,8 +102,23 @@ class Evolution:
                 for cr in self.creatures:
                     cr.search(self.meals)
                 return True
+        print()
+        returning = False
+        all_returned = True
+        all_dead_or_returned = True
+        for c in self.creatures:
+            if c.returning:
+                returning = True
+            if not c.returned and c.is_alive:
+                all_dead_or_returned = False
 
-        if all_return_or_dead:
+
+
+
+        if len(self.meals) == 0 and not returning:
+            return False
+
+        if all_dead_or_returned and not returning:
             return False
         pygame.display.update()
         time.sleep(1 / 30)
